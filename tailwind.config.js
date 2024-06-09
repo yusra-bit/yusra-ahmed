790931b9f54a3713bb04ebc454d8c583e7120814
Yusra Abdi <yusra4411@gmail.com>
2024-06-09 13:54:58 +0300

    added registration, login & email vertification

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/bg.jpg')"
+    },
   },
   plugins: [],
+}
 }
